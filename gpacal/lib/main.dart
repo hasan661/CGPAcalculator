92bc9993dcb477f0f9f6184ad44a21ecc0c6a5d7
Hasan <qhasan966@gmail.com>
2021-08-08 04:16:12 +0500

    dev/1

--- gpacal/lib/main.dart
+++ gpacal/lib/main.dart
@@ -1,33 +1,26 @@
 import 'package:flutter/material.dart';
 
 import 'gpainput.dart';
-import 'chart.dart';
+// import 'chart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<dynamic> gpa=[
-    
-
-  ];
+  final List<dynamic> gpa = [];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(0, 0, 128, 1),
-        accentColor: Color.fromRGBO(0, 0, 128, 1),
-      ),
-      home: Scaffold(
-
-          appBar: AppBar(
-            title: Text("CGPA Calculator"),
-          ),
-          body: Container(
-            child:
-            GpaInput(gpa),
-
-          )));
-            
-            
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(0, 0, 128, 1),
+          accentColor: Color.fromRGBO(0, 0, 128, 1),
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("CGPA Calculator"),
+            ),
+            body: Container(
+              child: GpaInput(gpa),
+            )));
   }
 }
